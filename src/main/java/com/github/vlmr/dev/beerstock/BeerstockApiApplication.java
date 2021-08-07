4f304ccefe8f888cf689f3a65b53b2b4cc755f60
vlmr-dev <vlmr.info@gmail.com>
2021-08-07 08:51:52 -0300

    Commit inicial

---
+++ src/main/java/com/github/vlmr/dev/beerstock/BeerstockApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.vlmr.dev.beerstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerstockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerstockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vlmr/dev/beerstock/BeerstockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vlmr.dev.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

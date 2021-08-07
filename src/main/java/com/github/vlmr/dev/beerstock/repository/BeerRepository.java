5311547d6eae0743f78be63f4d0df5129e1f7d47
vlmr-dev <vlmr.info@gmail.com>
2021-08-07 09:17:35 -0300

    Adiciona o package: repository e a interface: BeerRepository

---
+++ src/main/java/com/github/vlmr/dev/beerstock/repository/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.github.vlmr.dev.beerstock.repository;
+
+import com.github.vlmr.dev.beerstock.entity.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
+}

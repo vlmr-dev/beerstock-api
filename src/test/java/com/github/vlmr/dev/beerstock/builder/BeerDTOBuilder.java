118f15458aaa0df7310e0f808a50b72519b66e68
vlmr-dev <vlmr.info@gmail.com>
2021-08-15 22:45:33 -0300

    Adiciona o package: builder e a class: BeerDTOBuilder

---
+++ src/test/java/com/github/vlmr/dev/beerstock/builder/BeerDTOBuilder.java
@@ -0,0 +1,36 @@
+package com.github.vlmr.dev.beerstock.builder;
+
+import com.github.vlmr.dev.beerstock.dto.BeerDTO;
+import com.github.vlmr.dev.beerstock.enums.BeerType;
+import lombok.Builder;
+
+@Builder
+public class BeerDTOBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String name = "Brahma";
+
+    @Builder.Default
+    private String brand = "Ambev";
+
+    @Builder.Default
+    private int max = 50;
+
+    @Builder.Default
+    private int quantity = 10;
+
+    @Builder.Default
+    private BeerType type = BeerType.LAGER;
+
+    public BeerDTO toBeerDTO() {
+        return new BeerDTO(id,
+                name,
+                brand,
+                max,
+                quantity,
+                type);
+    }
+}
